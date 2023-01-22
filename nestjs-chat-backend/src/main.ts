7a55cea27eb1600c96dc9f3297882a7b989455ee
dezzerlol <misha.6325@gmail.com>
2023-01-22 22:03:17 +0500

    deploy fix

--- nestjs-chat-backend/src/main.ts
+++ nestjs-chat-backend/src/main.ts
@@ -5,14 +5,16 @@ import { AppModule } from './app.module'
 import * as cookieParser from 'cookie-parser'
 import { join } from 'path'
 
+const port = process.env.PORT || 5000
+
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule)
 
   app.useGlobalPipes(new ValidationPipe())
   /*  app.useGlobalGuards(new AtGuard()) */
 
-/*   app.useStaticAssets(join(__dirname, '..', 'static'))
- */
+  /*   app.useStaticAssets(join(__dirname, '..', 'static'))
+   */
   app.use(cookieParser())
 
   app.enableCors({
@@ -20,6 +22,6 @@ async function bootstrap() {
     origin: 'http://localhost:3000',
   })
 
-  await app.listen(5000)
+  await app.listen(port)
 }
 bootstrap()
